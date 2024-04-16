ad05f578718a3eab730852369f96a632f1d46da2
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-04-16 11:28:11 +0200

    Changing the tooken to be given thorugh env and making the main code a top level statement

--- Program.cs
+++ Program.cs
@@ -1,43 +1,37 @@
 ﻿using DSharpPlus;
 using DSharpPlus.SlashCommands;
 using Microsoft.Data.Sqlite;
+using rr_bot;
+using System;
 
-namespace rr_bot;
-
-internal class Program
+await using (var connection = new SqliteConnection("Data Source=database.db"))
 {
-    static async Task Main(string[] args)
-    {
-        await using (var connection = new SqliteConnection("Data Source=database.db"))
-        {
-            connection.Open();
-            var command = connection.CreateCommand();
-            command.CommandText =
-                @"CREATE TABLE IF NOT EXISTS `reactionRoles` ( `message` INTEGER NOT NULL, `emoji` TEXT NOT NULL, `role` INTEGER NOT NULL );";
-            command.ExecuteNonQuery();
-        }
-
-        String token = File.ReadAllText("token.conf").Trim();
-
-        // Setup Client
-        var clientConfig = new DiscordConfiguration
-        {
-            Token = token,
-            TokenType = TokenType.Bot,
-            Intents = DiscordIntents.GuildMessageReactions |
-                        DiscordIntents.AllUnprivileged
-        };
-
-        var discordClient = new DiscordClient(clientConfig);
-
-        // Register Slash Commands
-        discordClient.UseSlashCommands().RegisterCommands<ReactionRolesSlashCommands>();
-
-        // Bind Event Handlers
-        discordClient.MessageReactionAdded += ReactionRoleHandler.ReactionEventHandler;
-
-        // Launch Client
-        await discordClient.ConnectAsync();
-        await Task.Delay(-1);
-    }
+    connection.Open();
+    var command = connection.CreateCommand();
+    command.CommandText =
+        @"CREATE TABLE IF NOT EXISTS `reactionRoles` ( `message` INTEGER NOT NULL, `emoji` TEXT NOT NULL, `role` INTEGER NOT NULL );";
+    command.ExecuteNonQuery();
 }
+
+String token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+
+// Setup Client
+var clientConfig = new DiscordConfiguration
+{
+    Token = token,
+    TokenType = TokenType.Bot,
+    Intents = DiscordIntents.GuildMessageReactions |
+                DiscordIntents.AllUnprivileged
+};
+
+var discordClient = new DiscordClient(clientConfig);
+
+// Register Slash Commands
+discordClient.UseSlashCommands().RegisterCommands<ReactionRolesSlashCommands>();
+
+// Bind Event Handlers
+discordClient.MessageReactionAdded += ReactionRoleHandler.ReactionEventHandler;
+
+// Launch Client
+await discordClient.ConnectAsync();
+await Task.Delay(-1);
